7b9acc48e4fead4d16a7eb6067bce20d9aebace5
oyinkan <skusman@student.lautech.edu.ng>
2024-10-24 00:40:01 +0100

    worked on primary and secondary navigation bars

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaRegClock } from "react-icons/fa6";
+import { IoIosArrowUp } from "react-icons/io";
 
 const links = [
   {
     text: "Home",
-    href: "/home",
+    href: "/",
   },
   {
     text: "Account",
@@ -32,17 +35,27 @@ const links = [
 ];
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex justify-between py-4 px-[50px] bg-primary text-white">
       <div>
         {links.map((link) => (
-          <Link href={link.href} key={link.text} className="mr-5">
+          <Link
+            href={link.href}
+            key={link.text}
+            className={`mr-5 ${
+              pathname === link.href ? "opacity-70" : ""
+            } hover:opacity-70`}
+          >
             {link.text}
           </Link>
         ))}
       </div>
-      <div>
+      <div className="flex items-center">
+        <FaRegClock className="mr-2" />
         <Link href="#">Recently Viewed</Link>
+        <IoIosArrowUp className="ml-2" />
       </div>
     </nav>
   );
---
+++ src/components/primary-navbar/PrimaryNavbar.tsx
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PrimaryNavbar() {
+  return (
+    <div className="flex items-center px-6 py-4 shadow-md">
+      <div>
+        <Image
+          src="/images/logo.svg"
+          alt="Logo"
+          width={150}
+          height={40}
+          className="h-auto"
+        />
+      </div>
+      <div className="flex items-center">
+        <div>
+          <input
+            type="search"
+            placeholder="Electronic blender"
+            className="border border-solid w-96 h-12 p-6 rounded-[36px] relative"
+          />
+          <button className="bg-primary inline-block py-3 px-6 rounded-[36px] absolute ml-[-72px] mt-[3px]">
+            <Image
+              src="/images/search.svg"
+              alt="search icon"
+              width={20}
+              height={20}
+              className="h-auto"
+            />
+          </button>
+        </div>
+        <div className="flex items-center ml-12">
+          <Image
+            src="/images/download.svg"
+            alt="download icon"
+            width={40}
+            height={40}
+            className="h-auto"
+          />
+          <p className="ml-3">
+            Download the <br /> giftme app
+          </p>
+        </div>
+        <div className="flex items-center ml-12">
+          <Image
+            src="/images/welcome.svg"
+            alt="welcome icon"
+            width={40}
+            height={40}
+            className="h-auto"
+          />
+          <p className="ml-3">
+            Welcome <br />{" "}
+            <Link href="/">
+              <b>Sign In / Log In</b>
+            </Link>
+          </p>
+        </div>
+        <div className="flex items-center ml-12">
+          <Image
+            src="/images/cart.svg"
+            alt="cart icon"
+            width={40}
+            height={40}
+            className="h-auto"
+          />
+          <p className="ml-1 text-center">
+            <span className="bg-primary inline-block h-6 w-6 rounded-full text-white">
+              0
+            </span>
+            <br />
+            <Link href="/">Cart</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
